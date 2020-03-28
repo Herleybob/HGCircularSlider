804bb370803b82eb8093bbe0b4eb282b4be5cff0
Herleybob <56013441+Herleybob@users.noreply.github.com>
2020-03-28 11:02:49 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@ import PackageDescription
 
 let package2 = Package(
     name: "HGCircularSlider",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v8),],
     products: [
         .library(name: "HGCircularSlider", targets: ["HGCircularSlider"]),
     ],
     targets: [
         .target(name: "HGCircularSlider"),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.v5],
 )
