0792cfe688428628de183e4b226a95a69e9d9564
Herleybob <56013441+Herleybob@users.noreply.github.com>
2020-03-28 10:58:47 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,15 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
-let packages = Package(
+let package2 = Package(
     name: "HGCircularSlider",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v8)],
     products: [
         .library(name: "HGCircularSlider", targets: ["HGCircularSlider"]),
     ],
     targets: [
-        .target(name: "HGCircularSlider", path: "HGCircularSlider/Classes"),
-    ]
+        .target(name: "HGCircularSlider"),
+    ],
+    swiftLanguageVersions: [.v5]
 )
